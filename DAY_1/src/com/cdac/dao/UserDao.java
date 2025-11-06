b3c1202cdff2430c729e6a6407b54a65edeae0c9
Mukta Wagh <mukta3725@gmail.com>
2025-11-06 17:47:21 +0530

    Fix formatting and clean up UserDao interface

--- DAY_1/src/com/cdac/dao/UserDao.java
+++ DAY_1/src/com/cdac/dao/UserDao.java
@@ -6,6 +6,7 @@ import com.cdac.pojos.User;
 
 public interface UserDao {
 //sign in using layer
-	// ....... signIn(em,pass) throws SE
+	// ....... signIn(em,pass) throws SE 
 	User signIn(String email,String pwd) throws SQLException;
 }
+
